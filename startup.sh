db1f3bc9764d4a48742125414a85d9a09964558f
Kyle Rakos <rakoskr6@gmail.com>
2022-11-21 21:15:26 -0600

    adjusted startup to be called directly from a reboot

--- startup.sh
+++ startup.sh
@@ -1,2 +1,11 @@
+# Add the following to crontab -e: @reboot /home/pi/led_strips/startup.sh
+SHELL=/bin/sh
+PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin 
+
+sleep 5
+
 jack_control start
 alsactl --file /usr/share/doc/audioInjector/asound.state.RCA.thru.test restore
+
+#@reboot ./home/pi/led_strips/start_from_server.sh
+sh /home/pi/led_strips/start_from_server.sh
