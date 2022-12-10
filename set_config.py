996b283bcc5052068e54faca37bdbe08b9bb2f75
Kyle Rakos <rakoskr6@gmail.com>
2022-12-10 14:14:31 -0600

    Fixing set_config

--- set_config.py
+++ set_config.py
@@ -7,20 +7,22 @@ CONFIG_FILE='/home/pi/led_strips/config.json'
 
 def set_config(key, value):
     print("Setting value in config " + str((key, value)))
+    data = {}
     with open(CONFIG_FILE, 'r') as fp:
         data = json.load(fp)
         if key in data:
-            value = data.get(key)
-            if type(value) == bool:
-                casted = bool(value)
-            elif type(value) == float:
+            original_value = data.get(key)
+            if type(original_value) == bool:
+                casted = value.lower() == "true"
+            elif type(original_value) == float:
                 casted = float(value)
-            else
+            else:
                 casted = str(value)
             data[key] = casted
 
     with open(CONFIG_FILE, 'w') as fp:
-        fp.truncate()
+        print("Writing config")
+        print(json.dumps(data, indent=4))
         fp.write(json.dumps(data, indent=4))
 
 if len(sys.argv) != 3:
